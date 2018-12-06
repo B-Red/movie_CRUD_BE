72a2fb2d53f50a1bf5fd47143d5bacbdaa0c4566
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-12-06 14:42:45 -0700

    DB movies table populated

--- migrations/20181206131241_movies.js
+++ migrations/20181206131241_movies.js
@@ -1,15 +1,15 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('Movies', (movie) => {
+    return knex.schema.createTable('movies', (movie) => {
         movie.increments('id')
         movie.string('Title')
         movie.string('Director')
         movie.integer('Year')
-        movie.integer('My Rating')
-        movie.string('Poster URL')
+        movie.integer('My_Rating')
+        movie.string('Poster_URL')
     })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExist('Movies')
+    return knex.schema.dropTableIfExists('movies')
 };
