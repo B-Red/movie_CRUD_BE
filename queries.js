5c8662f5ee8e88419403a16db5b923770ef134dc
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-12-06 15:13:07 -0700

    queries updated

---
+++ queries.js
@@ -0,0 +1,29 @@
+const database = require('./database-connection')
+
+module.exports = {
+    //returns whole table.
+    listAll(){
+        return databases('movies')
+    },
+    //returns a single entity by 'id'
+    getById(id){
+        return database('movies').where('id', id)
+    },
+    //another way to get a single entity by 'id'
+    getById2(id){
+        return database('movies').where({ id : id }).first()
+    },
+    //Creates a new entity, and then returns whole table
+    createMovie(newMovie){
+        return database('movies').insert(newMovie).returning('*')
+    },
+    //Delete and entity
+    deleteStudent(id){
+        return database('movies').where('id', id).delete()
+    },
+    //updates an entity
+    updateMovie(id, movie){
+        return database('movies').where('id', id).update(movie).returning('*')
+    }
+
+}
